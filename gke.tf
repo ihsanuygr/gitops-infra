2999be0bac516a333d26099878853f33d6966015
ihsanuygr <cristofromuald@gmail.com>
2022-07-26 00:33:17 +0300

    added argocd-app

--- gke.tf
+++ gke.tf
@@ -27,38 +27,38 @@ resource "google_container_cluster" "primary" {
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-  remove_default_node_pool = true
-  initial_node_count       = 1
+  # remove_default_node_pool = true
+  initial_node_count       = 2
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
 }
 
 # Separately Managed Node Pool
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "${google_container_cluster.primary.name}-node-pool"
-  location   = var.gke_zone
-  cluster    = google_container_cluster.primary.name
-  node_count = var.gke_num_nodes
+# resource "google_container_node_pool" "primary_nodes" {
+#   name       = "${google_container_cluster.primary.name}-node-pool"
+#   location   = var.gke_zone
+#   cluster    = google_container_cluster.primary.name
+#   node_count = var.gke_num_nodes
 
-  node_config {
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+#   node_config {
+#     oauth_scopes = [
+#       "https://www.googleapis.com/auth/logging.write",
+#       "https://www.googleapis.com/auth/monitoring",
+#     ]
 
-    labels = {
-      env = var.project_id
-    }
+#     labels = {
+#       env = var.project_id
+#     }
 
-    preemptible  = true
-    machine_type = "e2-medium"
-    tags         = ["gke-node", "${var.project_id}-gke"]
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-  }
-}
+#     preemptible  = true
+#     machine_type = "e2-medium"
+#     tags         = ["gke-node", "${var.project_id}-gke"]
+#     metadata = {
+#       disable-legacy-endpoints = "true"
+#     }
+#   }
+# }
 
 
 # # Kubernetes provider
